577b917813f17fe3b6ee5a0890eff7db93e672b5
Itai Klapholtz <itaikla1@gmail.com>
2023-11-05 10:22:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ class SimpleWeight:
     @classmethod
     def zero(cls):
         return cls(False)
+
+    def toggle(self):
+        self.value_ = not self.value_
+
+    def copy(self):
+        return SimpleWeight(self.value_)
+
+    def __eq__(self, other):
+        if isinstance(other, SimpleWeight):
+            return self.value_ == other.value_
+        return False
